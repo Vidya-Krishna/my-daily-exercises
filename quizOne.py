a5921df55b591bcf842e9a5d184646191b531215
vidya_krishna <60732748+Vidya-Krishna@users.noreply.github.com>
2025-05-15 21:00:23 +0530

    Update quizOne.py

--- quizOne.py
+++ quizOne.py
@@ -9,5 +9,26 @@ options = (("[A] Iron", "[B] Chlorine", "[C] Bromine", "[D] Iodine"),
            ("[A] Carbohydrate", "[B] Calcium", "[C] Protein", "[D] Vitamin C"),
            ("[A] Chloroflurocarbon", "[B] Nitrogen dioxide", "[C] Sulphur dioxide", "[D] Carbon dioxide"),
            ("[A] Terminalia", "[B] Indica", "[C] Salix", "[D] Acacia"),)
-key = ("D","B","C","D","B")
+key = ("d","b","c","d","b")
+j_count = 0
+score  = 0
+for i in questions:
+      print()
+      print(i)
+      for j in options[j_count]:
+            print(j)
+      entered_option = input("Choose an answer: ").lower()
+      if entered_option == key[j_count]:
+            print('You nailed it!!!')
+            score += 1
+      else:
+            print('Better luck next question')
+            print(f'correct answer is {key[j_count]}')
+            
+      j_count += 1
+print('Your score is: ', score)
+
+#There are a bunch of things I want to add but those things are breaking this. 
+
+      
 
