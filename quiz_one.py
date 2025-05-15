38885d0a6d2fbb294836d2e99aaef1fa647aadfb
vidya_krishna <60732748+Vidya-Krishna@users.noreply.github.com>
2025-05-15 20:48:21 +0530

    Create quiz_one.py
    print() the question (only one element of this tuple) and options (total 1st row of this 2D tuple) 
    take user input and compare it to key (correct answers)
    keep count of number of and print score at last

---
+++ quiz_one.py
@@ -0,0 +1,13 @@
+#QUIZ 
+questions = (("Seaweeds are an important source of which of the following?"),
+      ("The ____ is the stem-like part of the leaf that joins the blade to the stem, is known as:"),
+      ("Among the given nutrients, milk is a poor source of which of the following?"),
+      ("Which gas is major contributor to greenhouse effect?"),
+      ("Which word is common in the botanical names of trees like Ashoka, Tamarind or Coral?"))
+options = (("[A] Iron", "[B] Chlorine", "[C] Bromine", "[D] Iodine"),
+           ("A] Vein",  "[B] Petiole",  "[C] Stipules","[D] Midrib"),
+           ("[A] Carbohydrate", "[B] Calcium", "[C] Protein", "[D] Vitamin C"),
+           ("[A] Chloroflurocarbon", "[B] Nitrogen dioxide", "[C] Sulphur dioxide", "[D] Carbon dioxide"),
+           ("[A] Terminalia", "[B] Indica", "[C] Salix", "[D] Acacia"),)
+key = ("D","B","C","D","B")
+
