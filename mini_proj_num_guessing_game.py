619003487450b2b9c779698ed0defac40bf6da9b
vidya_krishna <60732748+Vidya-Krishna@users.noreply.github.com>
2025-05-22 22:17:08 +0530

    Update mini_proj_num_guessing_game.py
    new edit: calculates only valid inputs

--- mini_proj_num_guessing_game.py
+++ mini_proj_num_guessing_game.py
@@ -6,20 +6,23 @@ def mini_proj_num_guess():
   high = 100
   cg = random.randint(low, high)
   gc = 0
-  invalid_gc = 0
   is_running = True
+  
   while is_running:
     n = input(f'Guess an int between {low}, {high}: ')
-    gc += 1
+
     if n.isdigit():
       n = int(n)
       if n < 0 or n > 100:
         print(f'range bound error, please enter an int between {low}, {high} ')
       elif n > cg:
+        gc += 1
         print('cold, try a lower number')
       elif n < cg:
+        gc += 1
         print('hot, try a high number')
       else:
+        gc += 1
         print(f'correct, it matched in {gc} times')
         is_running = False
     else:
