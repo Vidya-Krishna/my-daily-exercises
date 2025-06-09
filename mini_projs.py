699c15273c7905efe1b09c75159a70b6bc3c58e8
vidya_krishna <60732748+Vidya-Krishna@users.noreply.github.com>
2025-06-09 23:04:43 +0530

    Update mini_projs.py
    added 1/4 features

--- mini_projs.py
+++ mini_projs.py
@@ -1,3 +1,37 @@
+# CLI string operations code.
+def count_vowels(user_input_word):
+    # word_list = []
+    # for i in user_input_word: word_list.append(i)
+    vowels_occurences = {}
+    vowels = ["a", "e", "i", "o", "u"]
+    for i in vowels:
+        count = 0
+        for j in user_input_word:
+            if i == j: count += 1       
+        vowels_occurences[i] = count
+    print(vowels_occurences)
+    total = 0
+    for value in vowels_occurences.values():
+        print(value)
+        total += value
+    print("vowels = ", total)
+
+# wow = True
+# while wow:
+user_input_word = input("Enter a string to check vowel occurences: ").lower()
+print("~~~~~*** ** * ** ***~~~~~")
+print(user_input_word)#
+# print(f"{user_input_word} has {count_vowels(user_input_word)}")
+count_vowels(user_input_word)
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------
 #just another num_guessing code
 import random
 
